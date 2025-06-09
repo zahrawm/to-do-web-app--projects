53c20cc90228b535932ede7fe845867a1ca2ad03
Fatimah Zahra Adam <92226557+zahrawm@users.noreply.github.com>
2025-06-09 11:03:00 +0000

    codes updated

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ app.get('/', (req: Request, res: Response) => {
 
 
 app.use('/api/todos', todoRoutes);
+// app.use('/api/completed', completedRoutes);
 
 
 app.use((req: Request, res: Response) => {
